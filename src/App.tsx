af3f49b4a54154ca97ac71479980d8751a60117b
Tricia Amheiser <tricia@taarts.net>
2022-12-22 18:42:32 -0500

    init

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+// import { Button } from 'component-library-react'
+
+export type CategoryType = {
+  categoryId: string
+  title: string
+  description: string
+}
+
+export function App() {
+  return (
+    <div>
+      <h1>My App</h1>
+    </div>
+  )
+}
